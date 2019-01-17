c57042f0df65c143549f1861af65c2285dc9b1b2
tahorst <thorst@stanford.edu>
2019-01-16 17:27:57 -0800

    Create template.py

---
+++ template.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+'''
+Template file for new python files.
+'''
+
+import argparse
+import os
+import time
+
+
+FILE_LOCATION = os.path.dirname(os.path.realpath(__file__))
+
+
+def main():
+	'''
+	Main function
+	'''
+
+	return
+
+def parse_args():
+	'''
+	Parses arguments from the command line.
+
+	Returns:
+		ArgumentParser namespace: values of variables parsed from the command line
+	'''
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('-a', '--abc',
+		type=int,
+		default=1,
+		help='help')
+	parser.add_argument('--true',
+		action='store_true',
+		help='help')
+
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	start = time.time()
+
+	args = parse_args()
+
+	main()
+
+	print('Completed in {:.2f} min'.format((time.time() - start) / 60))

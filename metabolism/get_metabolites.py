a99da242e7bab84e6aa53c10927e92f791ca0be7
tahorst <thorst@stanford.edu>
2019-03-18 16:02:03 -0700

    Add kinetics

--- metabolism/get_metabolites.py
+++ metabolism/get_metabolites.py
@@ -1,6 +1,6 @@
 '''
 Uses raw_data and sim_data files to identify the metabolites with concentrations
-in the model.
+in the model and kinetic constraints used.
 
 Requires:
 	raw_data: cPickle object, specify path with RAW_DATA_FILE
@@ -8,29 +8,34 @@ Requires:
 
 Outputs:
 	metabolites.tsv: tsv file with a list of metabolites with concentrations
+	kinetics.tsv: tsv file with information about kinetic constraints used in the model
 '''
 
 import cPickle
 import csv
 import os
 
+from wholecell.utils import units
+
 
 FILE_LOCATION = os.path.dirname(os.path.realpath(__file__))
 BASE_DIR = os.path.dirname(FILE_LOCATION)
 RAW_DATA_FILE = os.path.join(BASE_DIR, 'scripts', 'raw_data.cp')
 SIM_DATA_FILE = os.path.join(BASE_DIR, 'scripts', 'sim_data.cp')
-OUTPUT_FILE = os.path.join(FILE_LOCATION, 'metabolites.tsv')
+METABOLITES_FILE = os.path.join(FILE_LOCATION, 'metabolites.tsv')
+KINETICS_FILE = os.path.join(FILE_LOCATION, 'kinetics.tsv')
 
 
+# Load data
 with open(RAW_DATA_FILE, 'rb') as f:
 	raw_data = cPickle.load(f)
 with open(SIM_DATA_FILE, 'rb') as f:
 	sim_data = cPickle.load(f)
 
+# Save metabolites
 metabolites = sim_data.process.metabolism.concDict.keys()
 bennett = [m['Metabolite'] for m in raw_data.metaboliteConcentrations]
-
-with open(OUTPUT_FILE, 'w') as f:
+with open(METABOLITES_FILE, 'w') as f:
 	writer = csv.writer(f, delimiter='\t')
 	writer.writerow(['Metabolite', 'Source'])
 
@@ -40,3 +45,17 @@ with open(OUTPUT_FILE, 'w') as f:
 		else:
 			source = 'Biomass'
 		writer.writerow([m, source])
+
+# Save kinetic constraints
+kinetic_constraints = sim_data.process.metabolism.constraintDict
+with open(KINETICS_FILE, 'w') as f:
+	writer = csv.writer(f, delimiter='\t')
+	writer.writerow(['Pubmed ID', 'Reaction ID', 'kcat (1/s)',
+		'KM (uM)', 'Adjusted kcat (1/s)', 'Enzyme',
+		'Substrates', 'Substrates for KM', 'Temperature (C)'])
+
+	for rxn in sorted(kinetic_constraints):
+		c = kinetic_constraints[rxn]
+		writer.writerow([c['Pubmed ID'], c['reactionID'], c['kcat'].asNumber(1 / units.s),
+			c['kM'], c['kcatAdjusted'].asNumber(1 / units.s), c['enzymeIDs'],
+			c['substrateIDs'], c['Concentration Substrates'], c['Temp']])

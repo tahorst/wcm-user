3bc6c14bdae8e40852041dbe5b6cb6bd937e296b
tahorst <thorst@stanford.edu>
2019-06-11 17:39:40 -0700

    Update cvxpy test script to use multiple solvers

--- random/test_cvxpy.py
+++ random/test_cvxpy.py
@@ -1,20 +1,53 @@
-# Test non-deterministic behavior between local and sherlock parca outputs
-# Includes necessary pieces to reproduce from fitPromoterBoundProbability from fit_sim_data_1.py
+'''
+Test non-deterministic behavior between local and sherlock parca outputs.
+Includes necessary pieces to reproduce from fitPromoterBoundProbability
+function from fit_sim_data_1.py.
+
+Results for solver options:
+  GLPK (original method): differences locally and in docker vs sherlock but gives 1.0 and 0.0 when expected
+  ECOS: gives comparable results and is faster (6-20x) but floating point issues for 0 and 1
+  SCS: gives comparable results and is faster (1-3x) but has floating point issues for 0 and 1
+  OSQP: gives different results and has floating point issues for 0 and 1 but is faster
+'''
 
 import os
+import time
 
 from cvxpy import Variable, Problem, Minimize, norm
 import numpy as np
 
 
+solvers = ['GLPK', 'ECOS', 'SCS', 'OSQP']
 cached_dir = os.path.join(os.path.dirname(__file__), 'test_cvxpy')
-
 
 # Parameters used in fitPromoterBoundProbability()
 PROMOTER_PDIFF_THRESHOLD = 0.1  # Minimum difference between binding probabilities of a TF in conditions where TF is active and inactive
 PROMOTER_REG_COEFF = 1e-3  # Optimization weight on how much probability should stay close to original values
 PROMOTER_SCALING = 10  # Multiplied to all matrices for numerical stability
 PROMOTER_NORM_TYPE = 1  # Matrix 1-norm
+
+
+def test_solver(problem, output, solver):
+	'''
+	Print output for each method along with time to solve
+
+	Args:
+		problem (cvxpy.Problem object): problem setup
+		output (cvxpy.Variable object): output variable to check
+		solver (str): solver method to use
+	'''
+
+	start = time.time()
+	problem.solve(solver=solver)
+	print('\n{} solved in {:.2f} s'.format(solver, time.time() - start))
+
+	sol = np.array(output.value).reshape(-1)
+
+	# Example indices for desired outputs, other indices can be checked
+	print(sol[114])  # Will be different locally vs sherlock
+	print(sol[0])  # Should be 1.0
+	print(sol[21])  # Should be 0.0
+
 
 # Reproduced from fitPromoterBoundProbability()
 k = np.load(os.path.join(cached_dir, 'k.npy'))
@@ -62,10 +95,6 @@ constraint_p = [
 
 # Solve optimization problem
 prob_p = Problem(objective_p, constraint_p)
-prob_p.solve(solver="GLPK")
 
-prob = np.array(P.value).reshape(-1)
-
-print(prob[114])  # will be different locally vs sherlock, along with other values
-
-import ipdb; ipdb.set_trace()
+for solver in solvers:
+	test_solver(prob_p, P, solver)

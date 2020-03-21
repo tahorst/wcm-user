c605879df805ff7c3b0cd9f81f58327222c475cc
tahorst <thorst@stanford.edu>
2020-03-21 14:04:22 -0700

    Initial FBA debugger script

---
+++ metabolism-debugger/README.md
@@ -0,0 +1,4 @@
+Debugger for running FBA problems outside the WCM framework.
+
+Branch to generate data in `timepoints/*/` (need to copy over individual timepoints): `save-met-data`
+Branch for `sim_data.cp`: `fun-met` (`4050d8d63`)
---
+++ metabolism-debugger/model_debug.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+
+"""
+Run FBA model from saved time points.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import cPickle
+import os
+
+from models.ecoli.processes.metabolism import FluxBalanceAnalysisModel
+from wholecell.utils import units  # required for proper cPickle load
+
+
+FILE_LOCATION = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(FILE_LOCATION, 'timepoints')
+SIM_DATA_FILE = os.path.join(FILE_LOCATION, 'sim_data.cp')
+
+
+def load_sim_data(path=SIM_DATA_FILE):
+	"""
+
+	"""
+
+	with open(path) as f:
+		sim_data = cPickle.load(f)
+	return sim_data
+
+def load_timepoints():
+	"""
+
+	"""
+
+	all_timepoints = []
+	for timepoint in os.listdir(DATA_DIR):
+		data = {}
+		timepoint_dir = os.path.join(DATA_DIR, timepoint)
+		for filename in os.listdir(timepoint_dir):
+			func = os.path.basename(filename).split('.')[0]
+			with open(os.path.join(timepoint_dir, filename), 'rb') as f:
+				data[func] = cPickle.load(f)
+
+		all_timepoints.append(data)
+
+	return all_timepoints
+
+def parse_args():
+	# type: () -> argparse.Namespace
+	"""
+	Parses arguments from the command line.
+
+	Returns:
+		values of variables parsed from the command line
+	"""
+
+	parser = argparse.ArgumentParser()
+
+	# parser.add_argument('-a', '--abc',
+	# 	type=int,
+	# 	default=1,
+	# 	help='help')
+	# parser.add_argument('--true',
+	# 	action='store_true',
+	# 	help='help')
+
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	args = parse_args()
+
+	# Load data from files
+	sim_data = load_sim_data()
+	timepoints = load_timepoints()
+	timepoint = timepoints[0]  # TODO: set up loop for all timepoints if desired
+
+	# Create model and extract relevant data
+	model = FluxBalanceAnalysisModel(sim_data)
+	water_idx = model.fba.getOutputMoleculeIDs().index('WATER[c]')
+
+	# TODO: setup loop to modify model to reach a desired output
+
+	# Setup model for timepoint
+	model.set_molecule_levels(*timepoint['set_molecule_levels'])
+	model.set_reaction_bounds(*timepoint['set_reaction_bounds'])
+	model.set_reaction_targets(*timepoint['set_reaction_targets'])
+
+	# Solve model and check outputs
+	print('Water change: {:.3f}'.format(model.fba.getOutputMoleculeLevelsChange()[water_idx]))

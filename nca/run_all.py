c49d0238b99f07506335b1b68df837b5ed9fef09
tahorst <thorst@stanford.edu>
2020-09-16 11:08:52 -0700

    Add option to compile log output files

--- nca/run_all.py
+++ nca/run_all.py
@@ -6,8 +6,11 @@ models of interest with the OPTIONS and MODELS variables.  Saves log files
 to log/ and uses the default output location for NCA results.
 """
 
+import argparse
+import csv
 import multiprocessing
 import os
+import re
 import subprocess
 from typing import List
 
@@ -15,6 +18,7 @@ from typing import List
 FILE_LOCATION = os.path.dirname(os.path.realpath(__file__))
 LOG_DIR = os.path.join(FILE_LOCATION, 'log')
 os.makedirs(LOG_DIR, exist_ok=True)
+SUMMARY_FILE = os.path.join(LOG_DIR, 'summary.tsv')
 
 OPTIONS = [
 	'average-seq',
@@ -38,15 +42,90 @@ def solve_nca(model: str, options: List[str]):
 	with open(os.path.join(LOG_DIR, f'{label}.log'), 'w') as f:
 		subprocess.run(cmd.split(), stdout=f)
 
+def compile_output():
+	"""
+	Compiles output log files to a spreadsheet.
+	"""
+
+	with open(SUMMARY_FILE, 'w') as f:
+		fieldnames = [
+			'Options',
+			'Overall match', 'Overall total', 'Overall percent',
+			'Negative match', 'Negative total', 'Negative percent',
+			'Positive match', 'Positive total', 'Positive percent',
+			'Error',
+			'r, all', 'n, all',
+			'r, consistent', 'n, consistent'
+			]
+		writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter='\t')
+		writer.writeheader()
+
+		for log_file in sorted(os.listdir(LOG_DIR)):
+			if not log_file.endswith('.log'):
+				continue
+
+			with open(os.path.join(LOG_DIR, log_file)) as log:
+				data = {'Options': log_file}
+				for line in log.readlines():
+					if line.startswith('Overall matches:'):
+						match, total = re.findall('([0-9]*)/([0-9]*)', line)[0]
+						percent = re.findall('([0-9]*\.[0-9]*)%', line)[0]
+						data['Overall match'] = int(match)
+						data['Overall total'] = int(total)
+						data['Overall percent'] = float(percent)
+					elif line.startswith('Negative regulation matches:'):
+						match, total = re.findall('([0-9]*)/([0-9]*)', line)[0]
+						percent = re.findall('([0-9]*\.[0-9]*)%', line)[0]
+						data['Negative match'] = int(match)
+						data['Negative total'] = int(total)
+						data['Negative percent'] = float(percent)
+					elif line.startswith('Positive regulation matches:'):
+						match, total = re.findall('([0-9]*)/([0-9]*)', line)[0]
+						percent = re.findall('([0-9]*\.[0-9]*)%', line)[0]
+						data['Positive match'] = int(match)
+						data['Positive total'] = int(total)
+						data['Positive percent'] = float(percent)
+					elif line.startswith('Error fitting original data (excluding unpredicted samples):'):
+						error = re.findall('([0-9]*\.[0-9]*)', line)[0]
+						data['Error'] = float(error)
+					elif line.startswith('All: r='):
+						r = re.findall('r=([0-9]*\.[0-9]*)', line)[0]
+						n = re.findall('n=([0-9]*)', line)[0]
+						data['r, all'] = float(r)
+						data['n, all'] = float(n)
+					elif line.startswith('Only if WCM consistent: r='):
+						r = re.findall('r=([0-9]*\.[0-9]*)', line)[0]
+						n = re.findall('n=([0-9]*)', line)[0]
+						data['r, consistent'] = float(r)
+						data['n, consistent'] = float(n)
+
+			writer.writerow(data)
+
+def parse_args() -> argparse.Namespace:
+	"""Parse command line args for options to run."""
+
+	parser = argparse.ArgumentParser()
+
+	# General options
+	parser.add_argument('-c', '--compile',
+		action='store_true',
+		help='Compile log output into a spreadsheet.')
+
+	return parser.parse_args()
 
 if __name__ == '__main__':
-	all_args = [
-		(model, [option for j, option in enumerate(OPTIONS) if i//2**j % 2 == 0])
-		for model in MODELS
-		for i in range(2**len(OPTIONS))
-		]
+	args = parse_args()
 
-	pool = multiprocessing.Pool(8)
-	results = [pool.apply_async(solve_nca, args) for args in all_args]
-	pool.close()
-	pool.join()
+	if args.compile:
+		compile_output()
+	else:
+		all_args = [
+			(model, [option for j, option in enumerate(OPTIONS) if i//2**j % 2 == 0])
+			for model in MODELS
+			for i in range(2**len(OPTIONS))
+			]
+
+		pool = multiprocessing.Pool(8)
+		results = [pool.apply_async(solve_nca, args) for args in all_args]
+		pool.close()
+		pool.join()

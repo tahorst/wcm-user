e448b5eb2f84d479a70ef8c5ae93ce586a87ee60
tahorst <thorst@stanford.edu>
2022-03-18 11:59:56 -0400

    Offset period to show edge effects

--- signal_period.py
+++ signal_period.py
@@ -15,30 +15,34 @@ FILE_LOCATION = os.path.dirname(os.path.realpath(__file__))
 OUTPUT_FILE = os.path.join(FILE_LOCATION, 'period.pdf')
 
 
-xmax = 10
-xsampled = 10000
-freq = xmax / xsampled
+def plot(axes, xmax):
+    xsampled = 10000
+    freq = xmax / xsampled
 
-x = np.linspace(0, xmax, xsampled)
-y = np.sin(0.4*np.pi*x) + np.sin(2*np.pi*x) + np.sin(10*np.pi*x) + np.sin(20*np.pi*x)  # freq of 0.2, 1, 5, 10
+    x = np.linspace(0, xmax, xsampled)
+    y = np.sin(0.4 * np.pi * x) + np.sin(2 * np.pi * x) + np.sin(10 * np.pi * x) + np.sin(20 * np.pi * x)  # freq of 0.2, 1, 5, 10
 
-f, Pxx = signal.periodogram(y, fs=1/freq)
-fft_f = np.fft.fftfreq(xsampled)
-fft_mag = np.real(np.fft.fft(y))
-fft_mask = fft_f > 0
+    f, Pxx = signal.periodogram(y, fs=1 / freq)
+    fft_f = np.fft.fftfreq(xsampled)
+    fft_mag = np.real(np.fft.fft(y))
+    fft_mask = fft_f > 0
 
-plt.figure()
+    ax_trace, ax_periodgram, ax_fft = axes
 
-plt.subplot(3, 1, 1)
-plt.plot(x, y)
+    ax_trace.plot(x, y)
 
-plt.subplot(3, 1, 2)
-plt.plot(f, Pxx)
-plt.xscale('log')
+    ax_periodgram.plot(f, Pxx)
+    ax_periodgram.set_xscale('log')
 
-plt.subplot(3, 1, 3)
-plt.plot(fft_f[fft_mask] / freq, fft_mag[fft_mask])
-plt.xscale('log')
+    ax_fft.plot(fft_f[fft_mask] / freq, fft_mag[fft_mask], label='power')
+    ax_fft.plot(fft_f[fft_mask] / freq, fft_mag[fft_mask] / fft_f[fft_mask] * freq, label='normalized')
+    ax_fft.set_xscale('log')
+    ax_fft.legend()
+
+_, axes = plt.subplots(3, 2, figsize=(6, 9))
+
+plot(axes[:, 0], 10)
+plot(axes[:, 1], 8)
 
 plt.tight_layout()
 plt.savefig(OUTPUT_FILE)
